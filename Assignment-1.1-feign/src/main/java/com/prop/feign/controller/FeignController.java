c59686583a186305c1b11166ca34ba7e4f22bbc9
Yaswanth <yaswanthmdh@gmail.com>
2021-11-11 16:44:22 +0530

    zipkin integration

--- Assignment-1.1-feign/src/main/java/com/prop/feign/controller/FeignController.java
+++ Assignment-1.1-feign/src/main/java/com/prop/feign/controller/FeignController.java
@@ -1,6 +1,8 @@
 package com.prop.feign.controller;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +14,15 @@ import com.prop.models.Teacher;
 
 @RestController
 public class FeignController {
-
+	
+	private static final Logger LOG = Logger.getLogger(FeignController.class.getName());
+	
 	@Autowired
 	FeignProxy proxyService;
 
 	@GetMapping("/feign/teacher")
 	public List<Teacher> getTeachers(@RequestHeader("Authorization") String authorization){
+		LOG.log(Level.INFO, "Get Teachers With feign client called...");
 		return proxyService.getAllTeachers(authorization);
 	}
 
